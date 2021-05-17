0910f93c6ce55e7bf0982bb3e8d740c14dfc9d91
teddy <teddyjh@protonmail.com>
2021-05-17 21:34:01 +0200

    add base code for the discord bot b0tty

---
+++ b0tty.py
@@ -0,0 +1,38 @@
+import discord
+from discord.ext import commands
+
+TOKEN = 'ODQxNzI1NjQ4OTU5MTc2NzY5.YJq8GQ.u4doeIELGPbraOX2fxpnJwX9mho'
+
+description = '''List of b0tty\'s commands'''
+bot = commands.Bot(command_prefix='!', description=description)
+
+def add2(int1 : float, int2: float):
+    return int1 + int2
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print('name: ' + bot.user.name)
+    print('id:   ' + bot.user.id)
+    print('------')
+
+
+@bot.command()
+async def hello(ctx):
+    """Says 'Hello there!'"""
+    await ctx.send("Hello there!")
+
+@bot.command()
+async def add(ctx, left : int, right : int):
+    """Adds two numbers together."""
+    await ctx.send(add2(left, right))
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("pong")
+
+@bot.command()
+async def Ping(ctx):
+    await ctx.send("Pong")
+
+bot.run(TOKEN)
